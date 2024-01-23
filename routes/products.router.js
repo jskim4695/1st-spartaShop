7881c5db84386884af08b8862be9524bdc069e06
JSKim <jskim4695@gmail.com>
2024-01-23 22:20:33 +0900

    Fix: 코드 정리

--- routes/products.router.js
+++ routes/products.router.js
@@ -6,97 +6,74 @@ const router = express.Router();
 /** 상품 등록 **/
 // localhost:3000/api/product POST
 router.post('/product', async (req, res) => {
-	const { title, content, writer, password } = req.body;
+	try {
+		if (!req.body) {
+			return res.status(400).json({
+				errorMessage: '데이터 형식이 올바르지 않습니다.',
+			});
+		}
 
-	const product = await Product.find({ title }).exec();
-	if (!product) {
-		return res.status(400).json({
-			success: false,
-			errorMessage: '데이터 형식이 올바르지 않습니다.',
+		const { title, content, writer, password } = req.body; // 구조분해할당
+		const newProduct = new Product({
+			title,
+			content,
+			writer,
+			password,
 		});
+		await newProduct.save();
+		res.status(201).json({ message: '판매 상품을 등록하였습니다.' });
+	} catch (error) {
+		res.status(500).json({ message: '예기치 못한 에러가 발생하였습니다.' });
 	}
-
-	const createproduct = await Product.create({
-		title,
-		content,
-		writer,
-		password,
-	});
-
-	return res.status(201).json({ message: '판매 상품을 등록하였습니다.' });
 });
 
 // 상품 목록 조회 API
-router.get('/products', async (req, res, next) => {
-	const products = await Product.find()
-		.select('title writer status regDate')
-		.sort('regDate')
-		.exec();
+router.get('/products', async (req, res) => {
+	try {
+		const products = await Product.find()
+			.select('_id title writer status regDate')
+			.sort('regDate: -1');
 
-	return res.status(200).json({ products });
+		return res.status(200).json({ products });
+	} catch (error) {
+		res.status(500).json({ message: '예기치 못한 에러가 발생하였습니다.' });
+	}
 });
 
 // 상품 상세 조회 API
-router.get('/products/:productId', async (req, res, next) => {
-	const { productId } = req.params;
+router.get('/products/:productId', async (req, res) => {
+	try {
+		const product = await Product.findById(req.params.productId).select(
+			'_id title content writer status regDate'
+		);
 
-	const product = await Product.findById(productId).exec();
-
-	if (!product) {
-		return res.status(404).json({
-			success: false,
-			message: '상품 조회에 실패하였습니다.',
-		});
+		if (!product) {
+			return res.status(404).json({
+				errorMessage: '상품 조회에 실패하였습니다.',
+			});
+		}
+		return res.status(200).json({ product });
+	} catch (error) {
+		res.status(500).json({ message: '예기치 못한 에러가 발생하였습니다.' });
 	}
-
-	return res.status(200).json({ product });
 });
 
 // 상품 정보 수정 API
-router.put('/products/:productId', async (req, res, next) => {
-	const { productId } = req.params;
-	const { title, content, password, status } = req.body;
-
-	const product = await Product.findById({ productId }).exec();
-
-	if (!product) {
-		return res
-			.status(404)
-			.json({ success: false, message: '상품 조회에 실패하였습니다.' });
-	}
-
-	if (password !== product.password) {
-		return res
-			.status(401)
-			.json({ success: false, message: '비밀번호가 일치하지 않습니다.' });
-	}
-
-	const updateProduct = await Product.findByIdAndUpdate(productId, {
-		title,
-		content,
-		status,
-		password,
-	}).exec();
-
-	return res.status(200).json({
-		success: true,
-		message: '상품정보를 수정하였습니다.',
-		updateProduct,
-	});
-});
-
-// 상품 삭제 API
-router.delete('/products/:productId', async (req, res, next) => {
-	const { productId } = req.params;
-	const { password } = req.body;
-
+router.put('/products/:productId', async (req, res) => {
 	try {
-		const product = await Product.findById(productId).exec();
+		if (!req.body || !req.params) {
+			return res.status(400).json({
+				errorMessage: '데이터 형식이 올바르지 않습니다.',
+			});
+		}
+
+		const { title, content, password, status } = req.body;
+		const product = await Product.findById(req.params.productId);
 
 		if (!product) {
-			return res
-				.status(404)
-				.json({ success: false, message: '상품 조회에 실패하였습니다.' });
+			return res.status(404).json({
+				errorMessage: '상품 조회에 실패하였습니다.',
+			});
 		}
 
 		if (password !== product.password) {
@@ -105,13 +82,46 @@ router.delete('/products/:productId', async (req, res, next) => {
 				.json({ success: false, message: '비밀번호가 일치하지 않습니다.' });
 		}
 
-		await Product.deleteOne({ _id: productId }).exec();
+		product.title = title;
+		product.content = content;
+		product.status = status;
 
-		return res
-			.status(200)
-			.json({ success: true, message: '상품을 삭제하였습니다.' });
+		await product.save();
+		res.json({ message: '상품정보를 수정하였습니다.' });
 	} catch (error) {
-		return res.status(500).json({ success: false, message: '서버오류' });
+		res.status(500).json({ message: '예기치 못한 에러가 발생하였습니다.' });
+	}
+});
+
+// 상품 삭제 API
+router.delete('/products/:productId', async (req, res) => {
+	try {
+		if (!req.body || !req.params) {
+			return res.status(400).json({
+				errorMessage: '데이터 형식이 올바르지 않습니다.',
+			});
+		}
+
+		const productId = req.params.productId;
+		const { password } = req.body;
+		const product = await Product.findById(req.params.productId);
+
+		if (!product) {
+			return res.status(404).json({
+				errorMessage: '상품 조회에 실패하였습니다.',
+			});
+		}
+
+		if (password !== product.password) {
+			return res
+				.status(401)
+				.json({ success: false, message: '비밀번호가 일치하지 않습니다.' });
+		}
+
+		await product.deleteOne({ id: productId });
+		res.json({ message: '상품을 삭제하였습니다.' });
+	} catch (error) {
+		res.status(500).json({ message: '예기치 못한 에러가 발생하였습니다.' });
 	}
 });
 
