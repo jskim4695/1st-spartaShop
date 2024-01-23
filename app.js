669671f81187a385d189ddae8a04e025df55b285
JSKim <jskim4695@gmail.com>
2024-01-23 11:20:50 +0900

    Feat: CRUD 구현, ignore/prettier 설정

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import connect from './schemas/index.js';
+import productRouter from './routes/products.router.js';
 
 const app = express();
 const PORT = 3000;
@@ -15,7 +16,7 @@ router.get('/', (req, res) => {
 	return res.json({ message: 'Hi!' });
 });
 
-app.use('/api', router);
+app.use('/api', [router, productRouter]);
 
 app.listen(PORT, () => {
 	console.log(PORT, '포트로 서버가 열렸어요!');
